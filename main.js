151dddb74d482d28565f1fd8e0bdc88467f185c6
Crystal K Young <maybiz@hotmail.com>
2020-10-29 07:16:47 -0400

    done

--- main.js
+++ main.js
@@ -2,6 +2,78 @@
  * YOUR CODE HERE *
  ******************/
 
+const onlyOdds = function(nums) {
+    return nums.filter(function(nums) {
+      return nums % 2 === 1
+    })
+  }
+
+
+const onlyEvens = function (nums) {
+  return nums.filter(function(num) {
+    return num % 2 === 0
+  })
+}
+
+const shortNamesOnly = function (names) {
+  return names.filter(function(name) {
+    return name.length < 7
+  })
+}
+
+const sNames = function (names) {
+  return names.filter(function(name) {
+    return name.startsWith('S')
+  })
+}
+
+const tripleAll = function (nums) {
+  return nums.map(function(num) {
+    return num * 3 
+  })
+}
+
+const negativeValues = function (nums) {
+  return nums.map(function(num) {
+  if (num > 0) {
+    return num * -1 
+  } else { 
+    return num
+  }
+  })
+}
+
+const questionings = function (str) {
+  return str.map(function(strs) {
+    return strs + '?'
+  })
+}
+
+const changeToInitials = function(str) {
+  return str.map(function(str) {
+      return str[0] + str[str.indexOf(' ')+1]
+  })
+}
+
+const doubleEven = function (nums) {
+  return nums.map(function(num) {
+    if (num % 2 === 0) {
+      return num * 2
+    } else {
+      return num
+    } 
+  })
+}
+
+const titleCaseNames = function (names) {
+  return names.map(function(name) {
+    return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()
+  })
+}
+
+const doubleEveryOther = function (nums) {
+  
+}
 
 
 /*********************************
